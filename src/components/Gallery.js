dfe87915c8138f45423ead8e4520bf0651d27cac
RafiUddin <rafiuddin.cse.pciu@gmail.com>
2022-12-14 17:08:37 +0600

    commit

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -4,27 +4,27 @@ import GalleryItem from "./GalleryItem";
 const images = [
   {
     id: 1,
-    src: "https://image-tc.galaxy.tf/wijpeg-8yyb89qrfrqk1fvnfmn9i6pqt/azalai-grand-hotel-pool_standard.jpg?crop=102%2C0%2C1717%2C1288",
-    title: "azalai grand hotel pool standard",
-    category: "pool",
+    src: "https://img.redbull.com/images/q_auto,f_auto/redbullcom/2021/4/24/hkpbfz2kr4j6bhy6agk3/carissa-moore-tahiti-2019",
+    title: "surfing waves: spilling waves",
+    // category: "pool",
   },
   {
     id: 2,
-    src: "https://www.hoteldekho.com/assets/images/bg4.jpg",
-    title: "No signal - no transmission",
-    category: "Hotel",
+    src: "https://www.lima2019.pe/sites/default/files/2019-07/surf-banner_0.jpg",
+    title: "plunging waves",
+    // category: "Hotel",
   },
   {
     id: 3,
-    src: "https://www.annabelle.com.cy/Resources/PageBanners/ANNABELLE/homepage-banner-1920x1244.jpg",
-    title: "Retro Closures",
-    category: "Annabelle",
+    src: "https://isasurf.org/wp-content/uploads/2022/08/background-adaptive.jpg",
+    title: "surging waves",
+    // category: "Annabelle",
   },
   {
     id: 4,
-    src: "https://file.videopolis.com/F/1/0e58f426-6927-4e39-9812-9be6df3fe331/101866.13348.cape-town.pepperclub-hotel.premium-overview-hMrhS2h0-57708-1280x720.jpeg",
-    title: "Vinyl Loveless Happiness",
-    category: "Vinyl Record",
+    src: "https://www.sixsenses.com/media/1552/fiji-surfing_fiji2_r.jpg?anchor=center&mode=crop&rnd=132266537910000000",
+    title: "collapsing waves",
+    category: "Record",
   },
 ];
 
